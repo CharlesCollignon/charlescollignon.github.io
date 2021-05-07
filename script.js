de5ea723bdd951489ddb3810b359d060501ef384
Charles Collignon <collignoncharles@gmail.com>
2021-05-07 20:25:30 +0200

    contact info animation

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const contact = document.querySelector(".contact-infos-clic");
 const infosCards = document.querySelector(".hidden");
+
+function handleClick() {
+  infosCards.classList.toggle("hidden");
+  contact.classList.toggle("hidden");
+}
+
+contact.addEventListener("click", handleClick);
