e63d780adc2f30a94efb907198b41b3b76e68ccd
Charles Collignon <collignoncharles@gmail.com>
2021-05-08 11:43:00 +0200

    added js effects

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const contact = document.querySelector(".contact-infos-clic");
 const infosCards = document.querySelector(".hidden");
+const arrowTop = document.querySelector(".fa-arrow-circle-up");
 
 function handleClick() {
   infosCards.classList.toggle("hidden");
   contact.classList.toggle("hidden");
 }
 
+function handleTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
 contact.addEventListener("click", handleClick);
+arrowTop.addEventListener("click", handleTop);
