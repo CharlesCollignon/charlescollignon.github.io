281094549dc72c50ed1a5c429d9dc8dfd37f1110
Charles Collignon <collignoncharles@gmail.com>
2021-05-08 16:28:00 +0200

    loading page

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const contact = document.querySelector(".contact-infos-clic");
 const infosCards = document.querySelector(".hidden");
 const arrowTop = document.querySelector(".fa-arrow-circle-up");
+// const loader = document.querySelector(".container-loader");
+
+// function myLoader() {
+//   loader.style.display = "none";
+// }
+$(window).on("load", function () {
+  $(".loading-page").duration(5000, function () {
+    $(".loading-page").fadeOut(1000, "linear");
+  });
+});
 
 function handleClick() {
   infosCards.classList.toggle("hidden");
