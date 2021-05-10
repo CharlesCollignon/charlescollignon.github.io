ef6f63efd73707b292f51ce6f37e7eb7a106fabd
Charles Collignon <collignoncharles@gmail.com>
2021-05-10 16:47:01 +0200

    finished loader and contact infos

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ $(btn).on("click", function () {
   $(".loading-page").fadeOut(1000, "linear");
 });
 
-function handleClick() {
-  infosCards.classList.toggle("hidden");
-  contact.classList.toggle("hidden");
-}
+$(contact).on("click", async function () {
+  $(".contacts").slideToggle(1000);
+});
+
+// function handleClick() {
+//   infosCards.classList.toggle("hidden");
+//   contact.classList.toggle("hidden");
+// }
 
 function handleTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-contact.addEventListener("click", handleClick);
+// contact.addEventListener("click", handleClick);
 arrowTop.addEventListener("click", handleTop);
