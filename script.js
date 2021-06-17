522fa4465c1b1792e6a14665c798f651ed206dd8
Charles Collignon <collignoncharles@gmail.com>
2021-06-17 20:53:02 +0200

    add video and images

--- script.js
+++ script.js
@@ -2,28 +2,26 @@ const contact = document.querySelector(".contact-infos-clic");
 const infosCards = document.querySelector(".hidden");
 const arrowTop = document.querySelector(".fa-arrow-circle-up");
 const btn = document.querySelector(".btn-enter");
+const loadingPage = document.querySelector(".loading-page")
 
 // function myLoader() {
 //   loader.style.display = "none";
 // }
-$(btn).on("click", function () {
-  $(".loading-page").fadeOut(1000, "linear");
-});
+// $(btn).on("click", function () {
+//   $(".loading-page").fadeOut(1000, "linear");
+// });
 
-$(contact).on("click", async function () {
-  $(".contacts").slideToggle(1000);
-});
+function handleClick() {
+  loadingPage.classList.add('zoomOut')
+}
 
-// function handleClick() {
-//   infosCards.classList.toggle("hidden");
-//   contact.classList.toggle("hidden");
-// }
+btn.addEventListener('click', handleClick)
+
 
 function handleTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-// contact.addEventListener("click", handleClick);
 arrowTop.addEventListener("click", handleTop);
 
 
